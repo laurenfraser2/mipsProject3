74a7808345788df97c191d9fe20c535a5656e7a2
laurenfraser2 <laurenfraser222@gmail.com>
2018-12-14 19:46:46 -0500

    Update project3.s
    deleting more uneeded parts of project2

--- project3.s
+++ project3.s
@@ -1,139 +1,51 @@
+
 .data
-    emptyInputMessage:	.asciiz "Input is empty."
-    tooLongMessage: .asciiz "Input is too long."
-    invalidBaseMessage:   .asciiz "Invalid base-34 number."
-    userInput:		.space 90000
+    emptyInput: .asciiz "Input is empty." #message for string with an empty input
+    longInput: .asciiz "Input it is too long." #messgae for string that has more than 4 characters
+    invalidInput: .asciiz "Invalid base-32 number." #message for string that includes one or more characters not in set
+    userInput: .space 1000
+
+
 .text
- main:
-	li $v0, 8       #Obtain user's input as text 
-	la $a0, userInput
-	li $a1, 90000
-	syscall
-	leadingSpaces:  #Remove leading spaces
-	 li $t8, 32      #Save space character to t8
-	 lb $t9, 0($a0)
-	 beq $t8, $t9, deleteSpace
-	 move $t9, $a0
-	deleteSpace: #initialize label to get rid of beginning spaces
-	 addi $a0, $a0, 1
-	 j checkLength
-	checkLength:   #find the length of the input
-	 addi $t0, $t0, 0  #start count at zero
-	 addi $t1, $t1, 10  #put character into t1
-	 add $t4, $t4, $a0  #keep what was originally in a0
-	lengthLoop:
-	 lb $t2, 0($a0)   #Load the next character to t2
-	 beqz $t2, endLoop   #End loop if null character is reached
-	 beq $t2, $t1, endLoop   #End loop if end-of-line is detected
-	 addi $a0, $a0, 1   #continue the string pointer
-	 addi $t0, $t0, 1
-	 j lengthLoop
-	endLoop:
-	 beqz $t0, emptyInput   #print empty input message if length is 0
-	 slti $t3, $t0, 5      #check whether or not count is > 4
-	 beqz $t3, tooLong #print out too long message if count > 4
-	 move $a0, $t4
-	 j isValid
-	emptyInput:
-	 li $v0, 4
-	 la $a0, emptyInputMessage
-	 syscall
-	 j exit
-	 tooLong:
-	  li $v0, 4
-	  la $a0, tooLongMessage
-	  syscall
-	  j exit
-	 isValid:
-	  lb $t5, 0($a0)
-	  beqz $t5, setUp  #End loop if null character is reached
-	  beq $t5, $t1, setUp  #End loop if end-of-line character is detected
-	  slti $t6, $t5, 48    #Character is less than 0 (ascii value of 48)
-	  bne $t6, $zero, invalidBase
-	  slti $t6, $t5, 58    #Character is less than or = to 9 (ascii value of 57)
-	  bne $t6, $zero, continue
-	  slti $t6, $t5, 65    #Check if the character is less than A (ascii value of 65)
-	  bne $t6, $zero, invalidBase
-	  slti $t6, $t5, 87    #Check if the character is less than V(ascii value of 86)
-	  bne $t6, $zero, continue
-	  slti $t6, $t5, 97    #Check if the character is less than a(ascii value 97)
-	  bne $t6, $zero, invalidBase
-	  slti $t6, $t5, 118   #Check if the character is less than v(ascii value 118)
-	  bne $t6, $zero, continue
-	  bgt $t5, 118, invalidBase   #Check if the character is greater than x(ascii value 118)
-	continue:
-	  addi $a0, $a0, 1
-	  j isValid
-	invalidBase:
-	  li $v0, 4
-	  la $a0, invalidBaseMessage
-	  syscall
-	  j exit
-	setUp:
-	  move $a0, $t4
-	  addi $t7, $t7, 0  #Initialize decimal sum to zero
-	  add $s0, $s0, $t0
-	  addi $s0, $s0, -1	
-	  li $s3, 3 #position 3
-	  li $s2, 2
-	  li $s1, 1
-	  li $s5, 0
-	convertString:
-	  lb $s4, 0($a0)
-	  beqz $s4, displaySum
-	  beq $s4, $t1, displaySum
-	  slti $t6, $s4, 58
-	  bne $t6, $zero, nums
-	  slti $t6, $s4, 87
-	  bne $t6, $zero, letters
-	  slti $t6, $s4, 118
-	  bne $t6, $zero, lowletters
-	 nums:
-	  addi $s4, $s4, -48
-	  j nextStep
-	 letters:
-	  addi $s4, $s4, -55
-	  j nextStep
-	 lowletters:
-	  addi $s4, $s4, -87
-	 nextStep:
-	  beq $s0, $s3, threePower
-	  beq $s0, $s2, twoPower
-	  beq $s0, $s1, onePower
-	  beq $s0, $s5, zeroPower
-	zeroPower:
-	  li $s6, 1
-	  mult $s4, $s6
-	  mflo $s7
-	  add $t7, $t7, $s7
-	onePower:
-	  li $s6, 32
-	  mult $s4, $s6
-	  mflo $s7
-	  add $t7, $t7, $s7
-	  addi $s0, $s0, -1
-	  addi $a0, $a0, 1
-	  j convertString
-	twoPower:
-	  li $s6, 1024
-	  mult $s4, $s6
-	  mflo $s7
-	  add $t7, $t7, $s7
-	  addi $s0, $s0, -1
-	  addi $a0, $a0, 1
-	  j convertString
-	threePower:
-	  li $s6, 32768 #32 to the 3rd power
-	  mult $s4, $s6
-	  mflo $s7
-	  add $t7, $t7, $s7
-	  addi $s0, $s0, -1
-	  addi $a0, $a0, 1
-	  j convertString
-        displaySum:
-	  li $v0, 1
-	  move $a0, $t7
-	  syscall
-	exit:
-	  li $v0, 10
-	  syscall
+    main:
+        li $v0, 8 #syscall to read string
+        la $a0, userInput #stores address of string
+        li $a1, 9000 #create ample space for string input
+        syscall
+        add $t0, $0, 0 #initialize $t0 register
+        add $t1, $0, 0 #initialize $t1 register
+
+        la $t2, userInput #load string addr into $t2 register
+        lb $t0, 0($t2)
+        beq $t0, 10, isEmpty #check for an empty string
+        beq $t0, 0 isEmpty
+
+        addi $s0, $0, 32 #store Base-N number
+        addi $t3, $0, 1 #iniialize new registers
+        addi $t4, $0, 0
+        addi $t5, $0, 0
+
+        skipSpace:
+            lb $t0, 0($t2) #load address in $t2 to $t0
+            addi $t2, $t2, 1 #increment pointer
+            addi $t1, $t1, 1 #increment counter
+            beq $t0, 32, skipSpace #jump to skipSpace branch if equal
+            beq $t0, 10, isEmpty #jump to isEmpty branch if equal
+            beq $t0, $0, isEmpty #jump to isEmpty branch if equal
+
+        viewChars:
+            lb $t0, 0($t2)
+            addi $t2, $t2, 1
+            addi $t1, $t1, 1
+            beq $t0, 10, goToBeg
+            beq $t0, 0, goToBeg
+            bne $t0, 32, viewChars
+
+        leftover:
+            lb $t0, 0($t2)
+            addi $t2, $t2, 1
+            addi $t1, $t1, 1
+            beq $t0, 10, goToBeg
+            beq $t0, 0, goToBeg
+            bne $t0, 32, isInvalid #jump to isInvalid branch if not equal
+            j leftover
