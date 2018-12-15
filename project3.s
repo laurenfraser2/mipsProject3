45f0db3aabfc37b7b654d0959e7c50da00a60c6b
laurenfraser2 <laurenfraser222@gmail.com>
2018-12-14 19:51:30 -0500

    new label for length/ introduce recursion

--- project3.s
+++ project3.s
@@ -58,3 +58,9 @@
             addi $t2, $t2, 1
             beq $t0, 32, continue
         addi $t2, $t2, -1
+        
+ stringLength:
+            lb $t0, ($t2)
+            addi $t2, $t2, 1
+            addi $t1, $t1, 1
+            beq $t0, 10, callconversionfunc
